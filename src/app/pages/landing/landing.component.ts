25f4cef962a581724ed32a0f2e4a2850fbffb201
kmbosworth321 <tug83382@temple.edu>
2017-07-23 19:42:39 -0400

    v1 landing page

---
+++ src/app/pages/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({
+  selector: 'landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css'],
+})
+
+export class LandingComponent {
+
+}
